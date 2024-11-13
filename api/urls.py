ad0c8c06665987d93fdd873eabe40bbdc4595de3
cmbak <chikmii@gmail.com>
2024-11-13 17:40:49 +0000

    Add signup page (ssr)

--- api/urls.py
+++ api/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
 
-from .views import main_spa
+from .views import main_spa, signup_spa
 
 urlpatterns = [
     path('', main_spa),
+    path('signup/', signup_spa, name='signup')
 ]
--- api/views.py
+++ api/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def main_spa(request: HttpRequest) -> HttpResponse:
     return render(request, 'api/spa/index.html', {})
+
+def signup_spa(request: HttpRequest) -> HttpResponse:
+    """View for user signup (using ssr)"""
+    return render(request, 'api/spa/signup.html', {})
