24277943ecc4aab9d7d38b450d93e35f179780ec
ec22697 <ec22697@qmul.ac.uk>
2024-11-17 15:02:09 +0000

    Override default user model and register user in admin
    Had to delete migrations since it's easier to change AUTH_USER_MODEL w/o any migrations being applied already

--- api/models.py
+++ api/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.utils.timezone import now
 
-# Create your models here.
-
 
 class PageView(models.Model):
     count = models.IntegerField(default=0)
