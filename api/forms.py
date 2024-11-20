913bcfa3ea14c9ce5de39f9b7975c676e2d0b4ae
ec22697 <ec22697@qmul.ac.uk>
2024-11-20 21:10:49 +0000

    Add min and max length for password and add more comments

--- api/forms.py
+++ api/forms.py
@@ -7,7 +7,7 @@ from .models import CustomUser
 
 class PasswordField(forms.CharField):
     """Password field for Django form - uses type password"""
-    widget = forms.PasswordInput
+    widget = forms.PasswordInput(attrs={'minlength': '8', 'maxlength': '30'})
 
 
 class DatePickerField(forms.DateField):
@@ -22,7 +22,9 @@ class SignupForm(ModelForm):
                    'confirm_password', 'date_of_birth', 'profile_picture']
 
     class Meta:
-        """Form is based on CustomUser model and has following fields from that model"""
+        """Form is based on CustomUser model
+        and has following fields from that model
+        """
         model = CustomUser
         fields = ['name', 'email', 'password',
                   'date_of_birth', 'profile_picture']
@@ -44,30 +46,24 @@ class SignupForm(ModelForm):
                  'placeholder': self.fields[field].label}  # Placeholder is same as input label
             )
 
-    def clean_email(self) -> str:
+    def clean_email(self):
+        """Validate that there isn't already an account with the same email"""
         email = self.cleaned_data['email']
         email_exists = CustomUser.objects.filter(email=email).exists()
         if email_exists:
             raise ValidationError('An account with that email already exists')
         return email
 
-    def clean_password(self) -> str:
+    def clean_password(self):
+        """Validate password length is between 8 and 30 inclusive"""
         password = self.cleaned_data['password']
         if len(password) < 8 or len(password) > 30:
             raise ValidationError(
                 "Your password must be between 8 and 30 characters")
         return password
 
-    def clean_confirm_password(self):
-        password = self.cleaned_data['password']
-        confirm_password = self.cleaned_data['confirm_password']
-
-        if confirm_password and password:
-            if confirm_password != password:
-                raise ValidationError("Passwords do not match")
-        return confirm_password
-
-    def clean_date_of_birth(self) -> datetime.date:
+    def clean_date_of_birth(self):
+        """Validate that the date of birth is not in the future"""
         dob = self.cleaned_data['date_of_birth']
         if dob is None:
             raise ValidationError("You must enter a birthday")
@@ -76,9 +72,21 @@ class SignupForm(ModelForm):
         return dob
 
     def clean_profile_picture(self):
+        """Validate that the image uploaded is a .png"""
         pic = self.cleaned_data['profile_picture']
-
         if pic:
             if pic.image.format != 'PNG':
                 raise ValidationError('You can only upload .png files')
         return pic
+
+    def clean(self):
+        """Validation of the form which needs multiple form fields"""
+        cleaned_data = super().clean()  # Keeps validation
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data['confirm_password']
+
+        # If both password fields are valid
+        if confirm_password and password:
+            if confirm_password != password:
+                self.add_error('password', 'Passwords do not match')
+                self.add_error('confirm_password', 'Passwords do not match')
