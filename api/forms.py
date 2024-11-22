1b0be8b61082cdb77948339315d31e1190ba0639
ec22697 <ec22697@qmul.ac.uk>
2024-11-22 11:27:49 +0000

    Add docstrings to form clean field methods

--- api/forms.py
+++ api/forms.py
@@ -18,7 +18,7 @@ class DatePickerField(forms.DateField):
 class SignupForm(ModelForm):
     """Django form used to sign up a user."""
     class Meta:
-        """Form is based on CustomUser model and has following fields from that model"""
+        """Form based on CustomUser model and has specified fields from that model"""
         model = CustomUser
         fields = ['name', 'email', 'password',
                   'date_of_birth', 'profile_picture']
@@ -40,21 +40,24 @@ class SignupForm(ModelForm):
                  'placeholder': field}  # Allows bootstrap form animation
             )
 
-    def clean_email(self) -> str:
+    def clean_email(self):
+        """Cleans email field - checks that there's no user with that email"""
         email = self.cleaned_data['email']
         email_exists = CustomUser.objects.filter(email=email).exists()
         if email_exists:
             raise ValidationError('An account with that email already exists')
         return email
 
-    def clean_password(self) -> str:
+    def clean_password(self):
+        """Cleans password field - checks that password is between 8 and 30 chars"""
         password = self.cleaned_data['password']
         if len(password) < 8 or len(password) > 30:
             raise ValidationError(
                 "Your password must be between 8 and 30 characters")
         return password
 
-    def clean_date_of_birth(self) -> datetime.date:
+    def clean_date_of_birth(self):
+        """Cleans d.o.b. field - checks that it's not in the future"""
         dob = self.cleaned_data['date_of_birth']
         if dob is None:
             raise ValidationError("You must enter a birthday")
@@ -63,8 +66,8 @@ class SignupForm(ModelForm):
         return dob
 
     def clean_profile_picture(self):
+        """Cleans profile picture field - checks that it's a png"""
         pic = self.cleaned_data['profile_picture']
-
         if pic:
             if pic.image.format != 'PNG':
                 raise ValidationError('You can only upload .png files')
