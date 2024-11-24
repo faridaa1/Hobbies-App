5e4fcab0618cc0aa3a6053af2a7e7239ed596da3
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-11-24 22:28:42 +0000

    defining interfaces

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,11 +1,27 @@
-export interface User {
-
+export interface CustomUser {
+    id: number;
+    name: string;
+    email: string;
+    date_of_birth: string;
+    hobbies: UserHobby[];
+    profile_picture: string | null
 }
 
 export interface Hobby {
+    id: number;
+    name: string;
+    description: string
+}
 
+export interface UserHobby { 
+    user: number;
+    hobby: number;
+    level: 'Beginner' | 'Intermediate' | 'Advanced';
+    start_date: string
 }
 
 export interface Friendship {
-    
+    user1: number;
+    user2: number;
+    status: 'Pending' | 'Accepted'
 }
