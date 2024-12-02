b1480fc7eaeb5912ac391c9c9ebd956ff61bbc77
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-12-02 13:09:00 +0000

    defining style

--- frontend/src/components/ProfilePage/FriendRequestsReceived.vue
+++ frontend/src/components/ProfilePage/FriendRequestsReceived.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="fs-4 mt-4 border rounded p-3 ps-5 mb-5 w-100">
-        <h1>Friend Requests Sent</h1>
+        <h1>Friend Requests Received</h1>
         <hr>
-        <div class="fs-4 mt-4 d-flex flex-rowalign-items-center gap-5 w-100" v-for="(friend, index) in friends">
+        <div class="fs-4 mt-4 d-flex flex-rowalign-items-center gap-4 w-100" v-for="(friend, index) in friends">
             <div class="d-flex gap-5 flex-row w-100 rounded p-2 align-items-center">
                 <img v-if="friend.profile_picture" style="width: 70px; height:70px; object-fit: cover;" class="rounded-circle" :src="friend.profile_picture" alt="">
                 <i v-if="!friend.profile_picture" class="bi bi-person-circle p-0" style="font-size: 70px; line-height: 0"></i>
                 <div class="p-2 rounded w-100">{{ friend.name }}</div>
             </div>
+            <button class="border-0 bg-primary text-white rounded px-3 fw-semibold" style="font-size: 1.1rem; height: 2.4rem;">
+                Accept
+            </button>
             <button class="border-0 bg-danger text-white rounded px-3 fw-semibold" style="font-size: 1.1rem; height: 2.4rem;">
-                Cancel
-                <!-- <i class="bi bi-trash-fill"></i> -->
+                Decline
             </button>
         </div>
     </div>
