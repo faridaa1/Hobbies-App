089aeb5b188d9ad191534f7a4ba5fa314c5d11ec
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-12-02 13:06:57 +0000

    defining style

--- frontend/src/components/ProfilePage/FriendRequestsSent.vue
+++ frontend/src/components/ProfilePage/FriendRequestsSent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fs-4 mt-4 border rounded p-3 ps-5 mb-5 w-100">
-        <h1>My Friends</h1>
+        <h1>Friend Requests Sent</h1>
         <hr>
         <div class="fs-4 mt-4 d-flex flex-rowalign-items-center gap-5 w-100" v-for="(friend, index) in friends">
             <div class="d-flex gap-5 flex-row w-100 rounded p-2 align-items-center">
@@ -8,8 +8,8 @@
                 <i v-if="!friend.profile_picture" class="bi bi-person-circle p-0" style="font-size: 70px; line-height: 0"></i>
                 <div class="p-2 rounded w-100">{{ friend.name }}</div>
             </div>
-            <button class="border-0 bg-primary text-white rounded px-3 fw-semibold" style="font-size: 1.1rem; height: 2.4rem;">
-                Remove
+            <button class="border-0 bg-danger text-white rounded px-3 fw-semibold" style="font-size: 1.1rem; height: 2.4rem;">
+                Cancel
                 <!-- <i class="bi bi-trash-fill"></i> -->
             </button>
         </div>
