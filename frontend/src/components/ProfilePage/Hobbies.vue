043a57c96dacd08c9586cbc4d9bbf3734b63b540
Farida Addo <f.addo@se22.qmul.ac.uk>
2024-12-02 11:50:50 +0000

    creating hobbies file

---
+++ frontend/src/components/ProfilePage/Hobbies.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="fs-4 mt-4 d-flex flex-row border rounded p-3 ps-5 align-items-center gap-5 w-100">
+        <div class="d-flex fs-5 gap-4 flex-column align-items-center w-100">
+            <div>
+                <img v-if="user.profile_picture" style="width: 200px; height:200px; object-fit: cover;" class="rounded-circle" :src="user.profile_picture" alt="">
+                <i v-if="!user.profile_picture" class="bi bi-person-circle"></i>
+            </div>
+            <div class="d-flex align-items-center">
+                <input class="w-75 mx-auto" type="file" @change="updateProfilePicture">
+            </div>
+        </div>
+        <div class="d-flex flex-column gap-3 w-100">
+            <div class="d-flex">
+                <div style="width: 10rem;">Full Name</div>
+                <input class="border border-secondary rounded px-2" type="text" disabled :value="user.name">
+                <button class="rounded mx-2 border-primary bg-primary text-white"><i class="bi bi-pencil"></i></button>
+            </div>
+            <div class="d-flex">
+                <div style="width: 10rem;">Email</div>
+                <input class="border border-secondary rounded px-2" type="text" disabled :value="user.email">
+                <button class="rounded mx-2 border-primary bg-primary text-white"><i class="bi bi-pencil"></i></button>
+            </div>
+            <div class="d-flex">
+                <div style="width: 10rem;">Password</div>
+                <input class="border border-secondary rounded px-2" type="text" disabled :value="user.password">
+                <button class="rounded mx-2 border-primary bg-primary text-white"><i class="bi bi-pencil"></i></button>
+            </div>
+            <div class="d-flex">
+                <div style="width: 10rem;">Date of Birth</div>
+                <input class="border border-secondary rounded px-2" type="text" disabled :value="user.date_of_birth">
+                <button class="rounded mx-2 border-primary bg-primary text-white"><i class="bi bi-pencil"></i></button>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+      import { defineComponent } from "vue";
+  
+      interface User {
+        profile_picture : string;
+        name : string;
+        email : string;
+        password : string;
+        date_of_birth : string;
+      }
+      
+      export default defineComponent({
+          data() {
+              return {
+                  title: "Other Page",
+                  user: {
+                    profile_picture : "https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649",
+                    name : "Person McPerson",
+                    email : "email@gmail.com",
+                    password : "password",
+                    date_of_birth : "01/01/2004",
+                  } as User
+              }
+          },
+          methods: {
+            updateProfilePicture(event: Event) {
+                const input: HTMLInputElement = event.target as HTMLInputElement
+                if (input && input.files && input.files[0]) {
+                    this.user.profile_picture = URL.createObjectURL(input.files[0])
+                }
+            }
+        },
+        async mounted() {
+            let response = await fetch("http://localhost:8000/profile/", {
+                credentials: 'include'
+            });
+            let data = await response.json();
+            // this.user = data
+            // const store = useUserStore()
+            // store.saveUser(user_id)
+          }
+      })
+  </script>
+  
+  <style scoped>
+  </style>
+  
