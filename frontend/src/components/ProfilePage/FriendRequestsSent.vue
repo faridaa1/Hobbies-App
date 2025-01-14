08073f7d30cb7aa1ea07576a0453d43b269092d8
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-01-14 21:28:59 +0000

    updating page size to 5 to be consistent with other pages

--- frontend/src/components/ProfilePage/FriendRequestsSent.vue
+++ frontend/src/components/ProfilePage/FriendRequestsSent.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="d-flex gap-2 justify-content-center">
             <button type="button" class="btn btn-secondary" v-if="friendIndex > 0" @click="prevPage">Previous</button>
-            <button type="button" class="btn btn-secondary" v-if="friendIndex + 10 < friends.length"
+            <button type="button" class="btn btn-secondary" v-if="friendIndex + pageSize < friends.length"
                 @click="nextPage">Next</button>
         </div>
     </div>
@@ -38,15 +38,15 @@ export default defineComponent({
             required: true
         }
     },
-    data(): { friendIndex: number } {
-        return { friendIndex: 0 }
+    data(): { friendIndex: number, pageSize: number } {
+        return { friendIndex: 0, pageSize: 5 }
     },
     methods: {
         prevPage(): void {
-            this.friendIndex -= 10
+            this.friendIndex -= this.pageSize
         },
         nextPage(): void {
-            this.friendIndex += 10
+            this.friendIndex += this.pageSize
         },
         async unsend(id: number): Promise<void> {
             if (useUserStore().csrf !== '') {
@@ -65,7 +65,7 @@ export default defineComponent({
                 }
                 useUserStore().updateFriendship(id, false)
                 if (this.displayedFriends.length === 0 && this.friends.length !== 0) {
-                    this.friendIndex -= 10
+                    this.friendIndex -= this.pageSize
                 }
             }
         }
@@ -79,7 +79,7 @@ export default defineComponent({
             }
             return []
         }, displayedFriends(): Friendship[] {
-            return this.friends.slice(this.friendIndex, this.friendIndex + 10)
+            return this.friends.slice(this.friendIndex, this.friendIndex + this.pageSize)
         }
     }
 })
