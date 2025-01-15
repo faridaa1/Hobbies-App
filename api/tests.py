4693c99cb1d61f55539412cd0fd882382dcc799a
chenjing <c.he@se22.qmul.ac.uk>
2025-01-15 14:16:45 +0000

    add profile pics for test users

--- api/tests.py
+++ api/tests.py
@@ -3,6 +3,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions 
 from api.models import CustomUser
@@ -36,7 +37,7 @@ class ProfileSeleniumTests(StaticLiveServerTestCase):
         # cls.selenium = WebDriver()
         cls.selenium = webdriver.Chrome(service=service)
         
- 
+
     @classmethod 
     def tearDownClass(cls):
         cls.selenium.quit()
@@ -126,11 +127,13 @@ class ProfileSeleniumTests(StaticLiveServerTestCase):
         # edit profile picture
         WebDriverWait(self.selenium, 10).until(
             expected_conditions.element_to_be_clickable((By.NAME, "remove_profile"))
-        ).click() 
-
+        ).click()
+        old_src = self.selenium.find_element(By.CSS_SELECTOR, "img.rounded-circle").get_attribute("src")
         profile_pic = self.selenium.find_element(By.NAME, "profile_pic")
         profile_pic.send_keys(valid_signup_data()['file_path2'])
-
+        WebDriverWait(self.selenium, 10).until(
+            lambda driver: driver.find_element(By.CSS_SELECTOR, "img.rounded-circle").get_attribute("src") != old_src)
+        
         # edit name
         self.selenium.find_element(By.NAME, "name_edit").click()
         name = self.selenium.find_element(By.NAME, "name")
